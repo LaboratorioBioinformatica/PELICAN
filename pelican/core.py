9316228f3ca479d851433ba4d2ecb6345caf1012
Fernando Rossi <fenobrerossi@gmail.com>
2026-06-17 14:13:38 -0300

    update to disable cuda

--- pelican/core.py
+++ pelican/core.py
@@ -192,8 +192,10 @@ from pathlib import Path
 # Force PyTorch to use CPU only
 # import os
 os.environ['TRITON_DISABLE'] = '1'  # Disable Triton JIT compiler
-os.environ['CUDA_VISIBLE_DEVICES'] = ''  # Hide CUDA devices
-os.environ['CUDA_LAUNCH_BLOCKING'] = '1'  # Force synchronous CUDA operations
+# os.environ['CUDA_VISIBLE_DEVICES'] = ''  # Hide CUDA devices
+# os.environ['CUDA_LAUNCH_BLOCKING'] = '1'  # Force synchronous CUDA operations
+os.environ['CUDA_VISIBLE_DEVICES'] = ''
+torch.cuda.is_available = lambda: False
 
 import torch
 
